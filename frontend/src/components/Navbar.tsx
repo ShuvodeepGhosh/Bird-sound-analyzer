a70167665dc99ce86ec3f0817675309f0c22abfe
ShuvodeepGhosh <dghosh711@gmail.com>
2026-07-05 11:30:24 +0530

    Feature Updates

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Box, Tooltip } from '@mui/material';
+import { AppBar, Toolbar, Button, Box, Tooltip, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
     <AppBar position="static" color="transparent" elevation={0} sx={{ borderBottom: '1px solid rgba(149, 213, 178, 0.2)' }}>
       <Toolbar>
-        <Box component={RouterLink} to="/" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
-          <img src="/logo.svg" alt="ChirpCheck Logo" style={{ height: '40px' }} />
+        <Box component={RouterLink} to="/" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+          <Typography variant="h6" style={{ fontWeight: 'bolder' }}>
+            ChirpCheck
+          </Typography>
         </Box>
         <Box>
-          <Button color="inherit" component={RouterLink} to="/">Home</Button>
           <Tooltip title="Coming soon">
             <span>
               <Button color="inherit" disabled sx={{ ml: 1 }}>
